043983a547fa394bf94a1b31182a7f604124cdd4
Manassarn Manoonchai <manassarn@gmail.com>
2022-03-06 23:34:09 +0700

    Add Middle component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,11 @@
-<script>
+<script lang="ts">
   import MainFullVh from "$lib/components/MainFullVh.svelte"
+  import Middle from "$lib/components/Middle.svelte"
 </script>
 
 <MainFullVh class="p-4">
-  <h1 class="text-3xl">Welcome to SvelteKit</h1>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  <Middle class="p-8 border rounded-xl">
+    <h1 class="text-3xl">Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  </Middle>
 </MainFullVh>
